736b44ff65232143c62d0237055c7583682d78fb
Kadu Moura <carlos.moura@i9xp.com.br>
2021-01-26 11:38:55 -0300

    Edit configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-    extends: 'standard',
-    parserOptions: {
-      ecmaVersion: 2017,
-      sourceType: 'module'
-    },
-    rules: {
-      // allow paren-less arrow functions
-      'arrow-parens': 0,
-      // allow async-await
-      'generator-star-spacing': 0,
-      // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-    },
-    env: {
-      node: true,
-      mongo: true,
-      mocha: true
-    }
+  extends: 'standard',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  env: {
+    node: true,
+    mongo: true,
+    mocha: true
+  },
+  globals: {
+    APIError: true
+  }
 }
